38b5c4a6d847ef04a04fe614bbd645d2549fa8f6
Garrett-B-P <77997734+Garrett-B-P@users.noreply.github.com>
2021-01-29 12:53:47 -0600

    Added my name

--- src/simonson/Main.java
+++ src/simonson/Main.java
@@ -3,6 +3,8 @@ package simonson;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Cajsa.");
+        System.out.println("Hello Cajsa, my name is Garrett.");
     }
 }
